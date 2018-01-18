d3a06a19262ea9a0d2d431483fbf59b84af06df0
Tiago Souza <tiagosouza@infojr.com.br>
2018-01-18 10:20:52 -0300

    README, index e style add

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+	<head lang = pt-br>
+		<meta charset="utf-8">
+		<title>Site Teste</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<h1>Ola, Bem-Vindo</h1>
+	</body>
+</html>
+		
